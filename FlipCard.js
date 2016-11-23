6b0362df031de37054d46c33ac0aa4dbc41dba80
yasemincidem <yasemincidem@gmail.com>
2016-11-23 15:57:45 +0300

    flip card class base component created

---
+++ FlipCard.js
@@ -0,0 +1,57 @@
+import React, {Component} from "react";
+import {Animated, Easing, TouchableOpacity, Text, View} from "react-native";
+
+class AvaFlipCard extends Component {
+    state = {
+        animatedValue: new Animated.Value(0),
+        isFlipped: true,
+    };
+
+    componentDidUpdate(prevProp, prevState) {
+        if (this.state.isFlipped !== prevState.isFlipped) {
+            this._flippedCard();
+        }
+    }
+
+    _flipToggleCard() {
+        this.setState({isFlipped: !this.state.isFlipped});
+    }
+
+    _flippedCard() {
+        Animated.spring(this.state.animatedValue, {
+            toValue: 0,   // Returns to the start
+            velocity: this.props.velocity,  // Velocity makes it move
+            tension: this.props.tension, // Slow
+            friction: this.props.friction,  // Oscillate a lot
+        }).start();
+    }
+
+    render() {
+        const rotateX = this.state.animatedValue.interpolate({
+            inputRange: [0, 0.5, 1],
+            outputRange: ['0deg', '360deg', '0deg']
+        });
+        return (
+            <TouchableOpacity onPress={this._flipToggleCard.bind(this)} style={styles.animatedContainer}>
+                <Animated.View
+                    style={[styles.animatedContainer,{transform: [{rotateX}]}]}>
+                    {this.flippedCardView(this.state.isFlipped)}
+                </Animated.View>
+            </TouchableOpacity>);
+    };
+
+
+    flippedCardView(isFlipped) {
+        if (isFlipped) {
+            return this.props.renderFront;
+        } else {
+            return this.props.renderBack;
+        }
+    }
+}
+const styles = {
+    animatedContainer: {
+        flex: 1
+    }
+}
+export {AvaFlipCard};
