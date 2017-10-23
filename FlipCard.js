e26ee4e119816f69d316f3de8f53ce0af636a98f
muyiwa-felixson <muyiwa.felixsonyusuf@ehealthafrica.org>
2017-10-23 22:02:47 +0100

    Passed appropriate value to activeOpacity prop

--- FlipCard.js
+++ FlipCard.js
@@ -1,11 +1,9 @@
-import React,{Component} from "react";
+import React, { Component } from 'react';
 import {
     Animated,
-    Easing,
-    TouchableOpacity,
-    Text,
-    View
-} from "react-native";
+    TouchableOpacity
+} from 'react-native';
+import PropTypes from 'prop-types';
 
 const styles = {
     animatedContainer: {
@@ -13,8 +11,8 @@ const styles = {
     }
 };
 
-class FlipCard extends Component{
-    constructor(){
+class FlipCard extends Component {
+    constructor() {
         super();
         this._flipToggleCard = this._flipToggleCard.bind(this);
         this._flippedCard = this._flippedCard.bind(this);
@@ -23,21 +21,31 @@ class FlipCard extends Component{
             animatedValue: new Animated.Value(0),
             isFlipped: true,
         };
-    };
-    componentWillReceiveProps(props){
+    }
+    static propTypes = {
+        flipped: PropTypes.bool,
+        clickable: PropTypes.bool,
+        velocity: PropTypes.number,
+        tension: PropTypes.number,
+        friction: PropTypes.number,
+        touchOpacity: PropTypes.number,
+        renderFront: PropTypes.object,
+        renderBack: PropTypes.object
+    }
+    componentWillReceiveProps(props) {
         this.setState({ isFlipped: props.flipped });
-    };
+    }
     componentDidUpdate(prevProp, prevState) {
         if (this.state.isFlipped !== prevState.isFlipped) {
             this._flippedCard();
         }
-    };
+    }
 
     _flipToggleCard() {
-        if (this.props.clickable){
-        this.setState({isFlipped: !this.state.isFlipped});
+        if (this.props.clickable) {
+            this.setState({ isFlipped: !this.state.isFlipped });
         }
-    };
+    }
 
     _flippedCard() {
         Animated.spring(this.state.animatedValue, {
@@ -46,35 +54,36 @@ class FlipCard extends Component{
             tension: this.props.tension, // Slow
             friction: this.props.friction,  // Oscillate a lot
         }).start();
-    };
+    }
     _getOpacity() {
         if (!this.props.touchOpacity) {
             return 0.2;
         }
         return this.props.touchOpacity;
     }
+
     render() {
         const rotateX = this.state.animatedValue.interpolate({
             inputRange: [0, 0.5, 1],
             outputRange: ['0deg', '360deg', '0deg']
         });
         return (
-            <TouchableOpacity activeOpacity={this._getOpacity} onPress={this._flipToggleCard} style={styles.animatedContainer}>
+            <TouchableOpacity activeOpacity={this._getOpacity()} onPress={this._flipToggleCard} style={styles.animatedContainer}>
                 <Animated.View
-                    style={[styles.animatedContainer,{transform: [{rotateX}]}]}>
+                    style={[styles.animatedContainer, { transform: [{ rotateX }] }]}
+                >
                     {this.flippedCardView(this.state.isFlipped)}
                 </Animated.View>
             </TouchableOpacity>);
-    };
+    }
 
 
     flippedCardView(isFlipped) {
         if (isFlipped) {
             return this.props.renderFront;
-        } else {
-            return this.props.renderBack;
         }
+        return this.props.renderBack;
     }
 }
 
-module.exports = FlipCard;
+export { FlipCard };
